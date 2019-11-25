1997160b38e3cd109a553718335fb745477d4b0a
Mikhail Altynov <altynov.m@gmail.com>
2019-11-25 10:05:45 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM tomcat:8.5.11-jre8
+COPY /target/Homework19.war /usr/local/tomcat/webapps/Homework19.war
---
+++ src/main/java/MyEJBean.java
@@ -0,0 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.ejb.Stateless;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+
+@Stateless(name = "MyBean")
+public class MyEJBean {
+
+    int a;
+    ArrayList<String> getDir(String dir) {
+        System.out.println();
+        ForkJoinPool forkJoinPool = new ForkJoinPool(10);
+        StringBuffer sb = new StringBuffer();
+        DirTask t = new DirTask(dir);
+        List<String> invoke = forkJoinPool.invoke(t);
+        ArrayList<String> SL = new ArrayList<>();
+        for (String s : invoke) {
+            if (s == null) {
+                continue;
+            }
+            SL.add(s);
+        }
+        return SL;
+    }
+}
